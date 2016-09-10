631f0dfbc649d4e654d703b66a89dce69002a494
Kitt Hodsden <kitt@hodsden.com>
2016-09-10 14:12:47 -0700

    Try another version of the rbenv install

--- vagrantBootstrap.sh
+++ vagrantBootstrap.sh
@@ -17,21 +17,18 @@ sudo apt-get -y install gcc make git git-core
 # and ruby stuff
 sudo apt-get -y install zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
-sudo apt-get -y install rbenv
-
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /etc/profile
-echo 'eval "$(rbenv init -)"' >> /etc/profile
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
 
-git clone https://github.com/rbenv/ruby-build.git
-cd ruby-build
-./install.sh
-exec $SHELL
-
-rbenv install 2.3.1
-rbenv global 2.3.1
-rbenv rehash
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+sudo -H -u vagrant bash -i -c 'rbenv install 2.3.1'
+sudo -H -u vagrant bash -i -c 'rbenv rehash'
+sudo -H -u vagrant bash -i -c 'rbenv global 2.3.1'
+sudo -H -u vagrant bash -i -c 'gem install bundler --no-ri --no-rdoc'
+sudo -H -u vagrant bash -i -c 'rbenv rehash'
 
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 sudo gem install bundler
